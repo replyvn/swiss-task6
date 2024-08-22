afb199a1d4a824413e4146aea40661783ebf194a
thangcoder <trangchongchengit@gmail.com>
2024-08-22 11:29:59 +0700

    first commit

---
+++ scripts/utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x32c6033fd1CC4DF21F0B079C2744ECbBADF31bF8'
+export const ProxyAdmin = '0xf2bF2d531AD765cd0d82bA12cc07F0c41F63cE21'
+export const SWTRImplementation = '0x9C7De47FB5f79De70e75D1e711d796781cdfC37e'
+    
